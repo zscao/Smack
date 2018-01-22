12d6c92617b7a0b21bb6edc462bcf5fcb9e027c1
Andy <zscao@msn.com>
2018-01-22 11:53:37 +1100

    built Channel view

---
+++ Smack/Views/GradientView.swift
@@ -0,0 +1,34 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by Zhengshan Cao on 22/1/18.
+//  Copyright © 2018 Zhengshan Cao. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
