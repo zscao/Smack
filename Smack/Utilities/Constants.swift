054e9f0e28347366d42a54b332474d89532ec2f9
Andy <zscao@msn.com>
2018-01-26 11:30:43 +1100

    added auth service

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,6 +8,20 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URLs
+let BASE_URL = "http://localhost:3005/v1"
+let URL_REGISTER = "\(BASE_URL)/account/register"
+
+
+// SEGUES
 let TO_LOGIN = "login"
 let TO_CREATE_ACCOUNT = "createAccount"
 let UNWIND_TO_CHANNEL = "unwindToChannel"
+
+// USER DEFAULTS
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
